b5edebdd3409d037284f470e779811e88651437a
hr5t5@windowslive.com <hr5t5@windowslive.com>
2018-12-30 10:06:12 +0100

    remove unnecessary constructors

--- src/game/board/model/collisions/interior-detector.ts
+++ src/game/board/model/collisions/interior-detector.ts
@@ -1,14 +1,9 @@
 import {CollisionDetector} from "./collision-detector";
-import {Iposition} from "../../../common/interfaces/position.interface";
 import SAT from "sat";
 
 
 
 export default class InteriorDetector extends CollisionDetector{
-
-    constructor(pos: Iposition) {
-        super(pos);
-    }
 
     protected handleResponse(res: SAT.Response): boolean {
         return !!(res.overlap >= 2 && res.a);
